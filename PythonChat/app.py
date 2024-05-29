1eddc05a1a8bfdcb900577399fe16f7ef36232d6
GenericJames <77215492+GenericJames@users.noreply.github.com>
2024-05-28 19:52:56 -0700

    Add files via upload

---
+++ PythonChat/app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+import webbrowser
+
+
+
+app = Flask(__name__)
+
+
+socketio = SocketIO(app, cors_allowed_origins = "*")
+
+@socketio.on("message")
+def sendMessage(message):
+    send(message, broadcast = True)
+
+
+
+@app.route("/")
+def message():
+    return render_template("message.html")
+
+if __name__ == "__main__":
+    app.run()   
